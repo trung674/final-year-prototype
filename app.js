102128207046139e58e2a6f058bb58cbf7b4918c
excalibur <thanhtrung674@gmail.com>
2016-09-07 14:08:54 +0100

    add style, ejs and simple nav bar

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 var express = require('express');
 var app = express();
+var sass = require('node-sass-middleware');
 
 app.use(require('./routes/index'));
 
+app.use(sass({
+    /* Options */
+    src: './public/sass',
+    dest: './public/stylesheets',
+    debug: true,
+    indentedSyntax: true,
+    outputStyle: 'compressed',
+    prefix:  '/stylesheets'
+}));
+
+app.use(express.static('./public'));
+app.set('view engine', 'ejs');
+app.set('views', './views');
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
