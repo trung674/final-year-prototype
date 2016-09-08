feb1da5f05bcc161b8b415ae1aba41bc2f69487f
trung674 <thanhtrung674@gmail.com>
2016-09-08 12:28:39 +0000

    add Event list, add functiont to read image folder

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+var path = require('path');
+var imageList = fs.readdirSync(path.join(__dirname, '../public/images'));
 
 router.get('/', function (req, res) {
-  res.render('index');
+  res.render('index', {
+      images: imageList
+  });
 });
 
 module.exports = router;
