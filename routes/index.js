75f8ad8beb7d6486ec3dab12b33972728a5ed7e5
trung674 <thanhtrung674@gmail.com>
2016-09-08 13:07:07 +0000

    add file name to alt attribute

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,18 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 var path = require('path');
+// Read image folder
+var imageNames = [];
 var imageList = fs.readdirSync(path.join(__dirname, '../public/images'));
+// Add each file name to imageName
+imageList.forEach(function(image, index){
+    imageNames.push(image.substr(0, image.lastIndexOf('.')) || image); // remove extension
+});
 
 router.get('/', function (req, res) {
   res.render('index', {
-      images: imageList
+      images: imageList,
+      name: imageNames
   });
 });
 
