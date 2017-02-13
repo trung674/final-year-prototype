d0cf70511883aeb05617c8a7f9385cfb1069119d
trung674 <thanhtrung674@gmail.com>
2017-02-13 13:25:09 +0000

    move session files to session folder, add link to each session

--- src/routes/user.js
+++ src/routes/user.js
@@ -7,15 +7,57 @@ const router = express.Router();
 
 module.exports = (passport) => {
   router.get('/user', isLoggedIn, (req, res) => {
-    Recording.find({}, (err, recordings) => {
-      if (err)
+    // Recording.find({}, (err, recordings) => {
+    //   if (err)
+    //     console.log(err);
+    //   res.render('user/user', {
+    //     recordings : recordings,
+    //     user : req.user, // get the user out of session and pass to template
+    //     moment : moment
+    //   });
+    // });
+    // Recording.find()
+    //   .then((recordings) => {
+    //     return User.find({records.status == 'ongoing'})
+    //       .then((ongoingRecords) {
+    //         return User.find({records.status == 'finished'})
+    //           .then((finishedRecords) {
+    //             res.render('user/user', {
+    //                 newRecordings : recordings,
+    //                 ongoingRecordings : ongoingRecords,
+    //                 finishedRecordings : finishedRecords,
+    //                 user : req.user, // get the user out of session and pass to template
+    //                 moment : moment
+    //               });
+    //           })
+    //       })
+    //   })
+
+    Recording.find()
+      .then((recordings) => {
+        res.render('user/user', {
+            newRecordings : recordings,
+            user : req.user,
+            moment : moment
+        });
+      })
+      .catch(err => {
         console.log(err);
-      res.render('user/user', {
-        recordings : recordings,
-        user : req.user, // get the user out of session and pass to template
-        moment : moment
-      });
-    });
+      })
+  });
+
+  router.get('/user/session/:session_id', isLoggedIn, (req, res) => {
+    Recording.findOne({_id: req.params.session_id})
+      .then((recording) => {
+        res.render('session/session', {
+            recording : recording,
+            user : req.user,
+            moment : moment
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      })
   });
 
   return router;
