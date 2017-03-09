675505dbfdc932d5fe968ab1f4f6e3a4c5680155
thanhtrung674 <thanhtrung674@gmail.com>
2017-03-09 15:54:35 +0000

    Upload recording to S3

--- src/record.js
+++ src/record.js
@@ -1,17 +1,29 @@
 import fs from 'fs-extra';
 import moment from 'moment';
+import AWS from 'aws-sdk';
+AWS.config.loadFromPath('./aws-config.json');
+const s3 = new AWS.S3();
+
+const bucketName = 'recording.uploads';
 
 function writeToDisk(audio) {
     let dateTime = moment().format('YYYYMMDDHHmm');
     const fileExtension = 'wav';
     const fileName = `${audio.word}_${audio.username}_${audio.recordingID}_${dateTime}.${fileExtension}`;
     const filePath = `./uploads/${audio.username}/${audio.recordingID}/${fileName}`;
+    const filePathAWS = `uploads/${audio.username}/${audio.recordingID}/${fileName}`;
     let fileBuffer;
     let dataURL = audio.dataURL.split(',').pop();
 
     fileBuffer = Buffer.from(dataURL, 'base64');
     fs.outputFileSync(filePath, fileBuffer);
-
+    s3.putObject({Bucket: bucketName, Key: filePathAWS , Body: fileBuffer, ContentEncoding: 'base64', ContentType: 'audio/wav'}, function(err, data) {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log('uploading to S3 successfully !');
+      }
+    });
     console.log('filePath', filePath);
 }
 
