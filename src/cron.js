124a20c6b5274011b7305c0ee18fb884b9e348f9
thanhtrung674 <thanhtrung674@gmail.com>
2017-03-18 23:50:35 +0000

    set up reminder now working

--- src/cron.js
+++ src/cron.js
@@ -1,25 +1,62 @@
 import moment from 'moment';
+import nodemailer from 'nodemailer';
 import Reminder from './models/reminder';
 const CronJob = require('cron').CronJob;
 
-const job = new CronJob('00 30 23 * * *', () => {
-  console.log('Start job');
-/*
- * Runs every weekday (Monday through Friday)
- * at 11:30:00 AM. It does not run on Saturday
- * or Sunday.
- */
- let now = moment().format();
- Reminder.find({date: {$lte: new Date(now)}})
-   .then((reminder) => {
-     console.log(reminder);
-   })
-   .catch((err) => {
-     console.log(err);
-   })
-}, function () {
-  /* This function is executed when the job stops */
-  console.log('Job finished !');
+// create reusable transporter object using the default SMTP transport
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.GMAIL_USERNAME,
+        pass: process.env.GMAIL_PASSWORD
+    }
+});
+
+// verify connection configuration
+transporter.verify(function(error, success) {
+   if (error) {
+        console.log(error);
+   } else {
+        console.log('Server is ready to take our messages');
+   }
+});
+
+const job = new CronJob('00 47 23 * * *', () => {
+    console.log('Start job');
+
+    let start = moment().startOf('day');
+    let end = moment().endOf('day');
+    Reminder.find({date: {$gte: start, $lt: end}}).populate('_user _recording')
+      .then((reminders) => {
+        reminders.forEach((reminder) => {
+            let mailOptions = {
+              from: '"Allen Walker" <allenwalker2160@gmail.com>', // sender address
+              to: 'thanhtrung674@gmail.com', // list of receivers
+              subject: 'Recording Reminder', // Subject line
+              text: `Hello ${reminder._user.information.fullname},
+                     This is a friendly reminder that you have set up before.
+                     Recording session: ${reminder._recording.title}
+                     Message: ${reminder.message}`, // plain text body
+              html: `Hello ${reminder._user.information.fullname}, <br />
+                     This is a friendly reminder that you have set up on ${reminder.createdAt}. <br />
+                     Recording session: ${reminder._recording.title} <br />
+                     Message: ${reminder.message}` // html body
+            };
+
+            // send mail with defined transport object
+            transporter.sendMail(mailOptions, (error, info) => {
+                if (error) {
+                    return console.log(error);
+                }
+            });
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
 },
-true, /* Start the job right now */
+false, /* Start the job right now */
 );
+
+job.start();
+console.log('job status', job.running); // job1 status
