f885dcf0300711e6c6d2efce22ba276b4f30c6a8
trung674 <thanhtrung674@gmail.com>
2017-04-09 15:59:03 +0100

    changes for heroku

--- src/record.js
+++ src/record.js
@@ -20,14 +20,14 @@ function writeToDisk(audio) {
     let dataURL = audio.dataURL.split(',').pop();
 
     // fileBuffer = Buffer.from(dataURL, 'base64');
-    // fs.outputFileSync(filePath, fileBuffer);
-    // s3.putObject({Bucket: bucketName, Key: filePathAWS , Body: fileBuffer, ContentEncoding: 'base64', ContentType: 'audio/wav'}, function(err, data) {
-    //   if (err) {
-    //     console.error(err);
-    //   } else {
-    //     console.log('uploading to S3 successfully !');
-    //   }
-    // });
+    fs.outputFileSync(filePath, fileBuffer);
+    s3.putObject({Bucket: bucketName, Key: filePathAWS , Body: fileBuffer, ContentEncoding: 'base64', ContentType: 'audio/wav'}, function(err, data) {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log('uploading to S3 successfully !');
+      }
+    });
     // console.log('filePath', filePath);
 }
 
